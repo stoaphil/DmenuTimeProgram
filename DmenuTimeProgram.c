1d85c871ae6891e10e004af62d37e2983425bf9f
stoaphil <stoa@gmx.us>
2016-11-30 14:40:14 -0600

    Add files via upload

---
+++ DmenuTimeProgram.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    time_t          t;
+    struct tm       *tmp;
+    char            buf[64];
+
+    time(&t);
+    tmp = localtime(&t);
+
+    if(strftime(buf, 64, "%X %Z, %a %D", tmp) == 0){ // See APUE fig. 6.11
+        printf("buffer length 64 too small\n");
+        exit(12);
+    }
+    else
+        printf("%s\n", buf);
+
+    exit(0);
+}
