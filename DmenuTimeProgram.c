4dedc99c8e712d06f1aa0be825552d7e660a821f
stoaphil <stoa@gmx.us>
2016-12-11 11:41:09 -0600

    Nonsense typo correction

--- DmenuTimeProgram.c
+++ DmenuTimeProgram.c
@@ -11,7 +11,7 @@ int main(void)
     time(&t);
     tmp = localtime(&t);
 
-    if(strftime(buf, 64, "%X %Z, %a %D", tmp) == 0){ // See APUE fig. 6.11
+    if(strftime(buf, 64, "%X %Z, %a %D", tmp) == 0){ //  See APUE fig. 6.11
         printf("buffer length 64 too small\n");
         exit(12);
     }
